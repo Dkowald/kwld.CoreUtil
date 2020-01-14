a3210b0dfbfd2e08850d29552b4443ffb263238e
dkowald <derek@kowald.id.au>
2020-01-14 14:48:10 +1000

    Added a Tee stream.

---
+++ src/kwd.CoreUtil.Tests/Streams/TeeStreamTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using kwd.CoreUtil.Streams;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kwd.CoreUtil.Tests.Streams
+{
+    [TestClass]
+    public class TeeStreamTests
+    {
+        [TestMethod]
+        public void WritesToAll()
+        {
+            var main = new MemoryStream();
+            var other = new MemoryStream();
+
+            var data = "Test text";
+
+            using var target = new TeeStream(main, other);
+
+            using var wr = new StreamWriter(target);
+            wr.Write(data);
+
+            wr.Flush();
+
+            Assert.AreEqual(data, Read(main));
+            Assert.AreEqual(data, Read(other));
+        }
+
+        [TestMethod]
+        public void WritesToAdded()
+        {
+            using var main = new MemoryStream();
+            using var other = new MemoryStream();
+
+            var target = new TeeStream(main, other);
+
+            using var wr = new StreamWriter(target);
+
+            wr.Write("Test");
+            wr.Flush();
+
+            var other2 = new MemoryStream();
+            target.Add(other2);
+
+            wr.Write(" after added");
+
+            wr.Flush();
+            
+            Assert.AreEqual("Test after added", Read(main));
+            Assert.AreEqual("Test after added", Read(other));
+            Assert.AreEqual(" after added", Read(other2));
+        }
+
+        [TestMethod]
+        public async Task WriteAsync()
+        {
+            var main = new MemoryStream();
+            var other = new MemoryStream();
+
+            await using var target = new TeeStream(main, other);
+
+            await using var wr = new StreamWriter(target);
+
+            var data = "Test data".AsMemory();
+            await wr.WriteAsync(data);
+
+            await wr.FlushAsync();
+
+            var other2 = new MemoryStream();
+            target.Add(other2);
+
+            await wr.WriteAsync(" after add");
+            await wr.FlushAsync();
+
+            Assert.AreEqual("Test data after add", Read(other));
+            Assert.AreEqual(" after add", Read(other2));
+        }
+
+        [TestMethod]
+        public void DisposeAll()
+        {
+            using var main = new MemoryStream();
+            using var other = new MemoryStream();
+
+            var target = new TeeStream(main, other);
+            using var wr = new StreamWriter(target);
+
+            wr.Dispose();
+
+            try
+            {
+                Read(main);
+                Assert.Fail("Expected stream to be closed.");
+            }catch(ObjectDisposedException){}
+
+        }
+
+        private static string Read(MemoryStream stream)
+        {
+            stream.Position = 0;
+            return new StreamReader(stream).ReadToEnd();
+        }
+
+    }
+}
---
+++ src/kwd.CoreUtil/Streams/TeeStream.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace kwd.CoreUtil.Streams
+{
+    /// <summary>
+    /// Write (only) to multiple streams with one stream
+    /// (aka unix Tee). 
+    /// </summary>
+    /// <remarks>
+    /// Only supports basic stream write functionality.
+    /// </remarks>
+    public class TeeStream : Stream
+    {
+        private readonly List<Stream> _out = new List<Stream>();
+
+        /// <summary>
+        /// Create a <see cref="TeeStream"/> that will
+        /// write to two streams at once.
+        /// </summary>
+        /// <param name="mainStream">Main stream</param>
+        /// <param name="otherStream">Other (branch) stream</param>
+        public TeeStream(Stream mainStream, Stream otherStream)
+        {
+            if(!mainStream.CanWrite)
+                throw new ArgumentException
+                    ("Must support write", nameof(mainStream));
+
+            if(!otherStream.CanWrite)
+                throw new ArgumentException
+                    ("Must support write", nameof(otherStream));
+
+            _out.Add(mainStream);
+            _out.Add(otherStream);
+        }
+
+        /// <summary>
+        /// Add additional streams to be written to.
+        /// </summary>
+        public TeeStream Add(params Stream[] otherStreams)
+        {
+            if(otherStreams.Any(x => !x.CanWrite))
+                throw new ArgumentException
+                    ("Must support write", nameof(otherStreams));
+    
+            _out.AddRange(otherStreams);
+
+            return this;
+        }
+
+        #region Stream
+
+        /// <inheritdoc />
+        public override void Close()
+        {
+            foreach (var item in _out)
+            {
+                item.Close();
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Flush()
+        {
+            foreach (var item in _out)
+            {
+                item.Flush();
+            }
+        }
+
+        /// <inheritdoc />
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            return Task.WhenAll(
+                _out.Select(x => x.FlushAsync(cancellationToken)));
+        }
+        
+        /// <inheritdoc />
+        public override Task WriteAsync(byte[] buffer, int offset, int count, 
+            CancellationToken cancellationToken)
+        {
+            return Task.WhenAll(_out.Select(x => 
+                x.WriteAsync(buffer, offset, count, cancellationToken)));
+        }
+
+        /// <inheritdoc />
+        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, 
+            CancellationToken cancellationToken = default)
+        {
+            foreach (var item in _out)
+            {
+                await item.WriteAsync(buffer, cancellationToken);
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Write(ReadOnlySpan<byte> buffer)
+        {
+            foreach (var item in _out)
+            {
+                item.Write(buffer);
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            foreach (var item in _out)
+            {
+                item.Write(buffer, offset, count);
+            }
+        }
+        
+        /// <inheritdoc />
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException("Cannot Read");
+        }
+
+        /// <inheritdoc />
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException("Cannot Seek");
+        }
+
+        /// <inheritdoc />
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException("Cannot set length: seek not supported");
+        }
+
+        /// <inheritdoc />
+        public override bool CanRead => false;
+
+        /// <inheritdoc />
+        public override bool CanSeek => false;
+
+        /// <inheritdoc />
+        public override bool CanWrite => true;
+
+        /// <summary>
+        /// Gets the length in bytes of the main (first) stream.
+        /// </summary>
+        public override long Length => _out.First().Length;
+        
+        /// <summary>
+        /// Gets the current position in the main (first) stream.
+        /// </summary>
+        public override long Position 
+        { 
+            get => _out.First().Position;
+            set => throw new NotImplementedException("Cannot seek");
+        }
+        #endregion
+
+    }
+}
